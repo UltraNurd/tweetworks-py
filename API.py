2df11fa7dbf852867ce1ab0d80699a6389af77ec
Nicolas Ward <ultranurd@yahoo.com>
2009-06-21 03:02:23 -0400

    improved error information; added HTTP authentication; tested first authenticating API call

--- API.py
+++ API.py
@@ -20,6 +20,8 @@ import Post
 class API:
     """
     Implement the Tweetworks API with HTTP POSTs.
+
+    In general, API methods are named for their URL.
     """
 
     class TweetworksException(Exception):
@@ -27,17 +29,39 @@ class API:
         Errors in handling Tweetworks API calls.
         """
 
-        def __init__(self, value):
-            self.parameter = value
+        def __init__(self, value, url = ""):
+            # Store the URL that caused the error
+            self.url = url
+
+            # Combine the URL with the error message
+            if url != "":
+                self.parameter = "%s: %s" % (url, value)
+            else:
+                self.parameter = value
+
         def __str__(self):
             return repr(self.parameter)
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, username = "", password = ""):
         """
-        We need a Tweetworks API key to send requests.
+        We need a Tweetworks API key to send requests. Optionally specify a
+        Tweetworks username and password to use those methods that require
+        authentication.
         """
 
+        # Store the developer's API that will be used to make requests
         self.api_key = api_key
+
+        # If a username and password were specified, set up authentication
+        if username != "" and password != "":
+            password_mgr = urllib2.HTTPPasswordMgr()
+            password_mgr.add_password("Login please",
+                                      "http://www.tweetworks.com",
+                                      username, password)
+            auth_handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+            self.auth = urllib2.build_opener(auth_handler)
+        else:
+            self.auth = None
 
     def request(self, url, data = {}):
         """
@@ -49,9 +73,20 @@ class API:
         data["data[key]"] = self.api_key
         encoded_data = urllib.urlencode(data)
 
-        # Construct the request and read the response
+        # Construct the request
         request = urllib2.Request(url, encoded_data)
-        response = urllib2.urlopen(request)
+
+        # Check for HTTP request errors
+        try:
+            # Use an authenticating URL opener if necessary
+            if self.auth != None:
+                response = self.auth.open(request)
+            else:
+                response = urllib2.urlopen(request)
+        except urllib2.HTTPError, e:
+            # Authentication error
+            if e.code == 401:
+                raise API.TweetworksException("Authentication required", url)
 
         # Parse the XML response
         xml_response = lxml.etree.parse(response)
@@ -60,7 +95,7 @@ class API:
         # Check for errors
         error = xml_response.xpath("/error/text/text()")
         if len(error) > 0:
-            raise API.TweetworksException(error[0])
+            raise API.TweetworksException(error[0], url)
 
         # Return the response XML
         return xml_response
@@ -79,6 +114,18 @@ class API:
         # Return the read posts
         return posts
 
+    def contributed_posts(self, username, recent = False):
+        """
+        Retrieves all posts contributed by the specified user; optionally only
+        recently updated posts. Posts are returned in descending order by date.
+        """
+
+        # Format the request URL
+        url = "http://www.tweetworks.com/posts/contributed/%s/%s.xml" % (username, ("newest", "updated")[recent])
+
+        # Read the posts from the response XML
+        return self.read_post_xml(self.request(url))
+
     def view_posts(self, id):
         """
         Retrieves a single discussion and threaded list of replies.
@@ -88,4 +135,11 @@ class API:
         url = "http://www.tweetworks.com/posts/view/%d.xml" % id
 
         # Read the posts from the response XML
-        return self.read_post_xml(self.request(url))
+        posts = self.read_post_xml(self.request(url))
+        if len(posts) == 1:
+            # Return the single post
+            return posts[0]
+        else:
+            # Something weird happened to result in non-unary post result
+            raise API.TweetworksException("%d posts were returned" % len(posts),
+                                          url)
