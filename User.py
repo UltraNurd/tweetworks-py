5ccd71983895aa104a994af5decce81a5fe7ab33
Nicolas Ward <ultranurd@yahoo.com>
2009-07-04 15:44:41 -0400

    added GPL license, tweetworks copyright notice

--- User.py
+++ User.py
@@ -1,9 +1,40 @@
+# -*- coding: utf-8 -*-
 """
 Read Tweetworks API users from XML responses.
 
 Nicolas Ward
 @ultranurd
+ultranurd@yahoo.com
+http://www.ultranurd.net/code/tweetworks/
 2009.06.19
+"""
+
+"""
+This file is part of the Tweetworks Python API.
+
+Copyright © 2009 Nicolas Ward
+
+Tweetworks Python API is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tweetworks Python API is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Tweetworks Python API. If not, see http://www.gnu.org/licenses/
+
+The term "Tweetworks" is Copyright © 2009 Tweetworks, LLC and is used
+under license. See http://www.tweetworks.com/pages/terms
+
+The use of this software requires a unique Tweetworks API key. You must be a
+registered Tweetworks user, and have received an API key after requesting one
+via http://www.tweetworks.com/pages/contact.
+
+The term "Twitter" is Copyright © 2009 Twitter, Inc.
 """
 
 # System includes
